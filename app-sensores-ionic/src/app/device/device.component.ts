087e9bad219cc3fba2bc27d1e4ae46953a29c290
Gustavo Siciliano <gussiciliano@gmail.com>
2023-04-13 13:45:01 -0300

    fix for component ionic

--- app-sensores-ionic/src/app/device/device.component.ts
+++ app-sensores-ionic/src/app/device/device.component.ts
@@ -1,11 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+// import { ChangeDetectionStrategy, Component, inject, Input } from '@angular/core';
+// import { RouterLink } from '@angular/router';
+// import { IonicModule, Platform } from '@ionic/angular';
 import { Device } from '../interfaces/device';
 import { DeviceService } from '../services/device.service';
+
 
 @Component({
   selector: 'app-device',
   templateUrl: './device.component.html',
   styleUrls: ['./device.component.css'],
+  //changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
+  imports: [
+    CommonModule,
+    //IonicModule,
+    //RouterLink
+  ],
 })
 export class DeviceComponent {
 
@@ -13,4 +25,5 @@ export class DeviceComponent {
     private deviceService: DeviceService) {}
 
   devices: Device[] = this.deviceService.getDevices();
+  palabras: String[] = this.deviceService.getPalabras();
 }
